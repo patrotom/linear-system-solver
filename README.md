df5555b38cfd3132949c0c80122d34da57e75c02
Tomas Patro <tomas.patro@gmail.com>
2020-12-20 16:12:07 +0100

    Add documentation

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # System of Linear Equations Solver
 
-`TODO`
+The repository contains a simple implementation of two iterative methods to solve the system of linear equations:
+
+* Jacobi method
+* Gauss-Seidel method
+
+The code is located in the [lyn_sys_solver.py](lyn_sys_solver.py) script. It is a simple **Python 3** script. To run it:
+
+``` shell
+chmod 755 lyn_sys_solver.py
+./lyn_sys_solver.py
+```
+
+You have to have Python 3 installed on your computer. Also, you have to install the `numpy` module prior to running it since this is a prerequisite used in the script.
--- lyn_sys_solver.py
+++ lyn_sys_solver.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import abc
-import sys
 import numpy as np
 from numpy import linalg as la
 
 
 class Solution():
+    """
+    The Solution class is a placeholder for the solution of the system of linear
+    equations.
+    """
     def __init__(self, found, it_num, xk):
         self.found = found
         self.it_num = it_num
@@ -14,7 +17,18 @@ class Solution():
 
 
 class LynSysSolver(abc.ABC):
+    """
+    The LynSysSolver class is an abstract class which contains the general
+    behavior of the iterative methods for solving the linear systems.
+    """
     def __init__(self, gamma, n, rel_prec, K):
+        """
+        * gamma - used as parameter for construction of the matrix A
+        * n - size of the square matrix A
+        * rel_prec - relative precision which is used to determine the validity
+        of solution
+        * K - maximum number of iterations
+        """
         self.gamma = gamma
         self.n = n
         self.rel_prec = rel_prec
@@ -23,6 +37,11 @@ class LynSysSolver(abc.ABC):
         self.init_Q()
 
     def solve(self):
+        """
+        Solves the given linear system using an iterative method. If we do not
+        find the solution after K iterations, we return the Solution object with
+        the attribute found set to False.
+        """
         prev_xk = self.x0
 
         for k in range(self.K):
@@ -36,6 +55,9 @@ class LynSysSolver(abc.ABC):
         return Solution(False, K, None)
 
     def init_input(self):
+        """
+        Initializes the matrix A, vector b, and initial vector x0.
+        """
         m1 = np.diagflat([-1] * (self.n - 1), -1)
         m2 = np.diagflat([-1] * (self.n - 1), 1)
         m3 = np.diagflat([self.gamma] * self.n)
@@ -52,26 +74,54 @@ class LynSysSolver(abc.ABC):
         )
 
     def norm(self, arr):
+        """
+        Calculates the maximum norm of a matrix or vector.
+        """
         return la.norm(arr, np.inf)
     
     def calc_xk(self, prev_xk):
+        """
+        Calculates a current solution vector xk using the given formula:
+        
+        Q^(−1) * ((Q − A) * x_{k−1} + b)
+        """
         return la.inv(self.Q) * ((self.Q - self.A) * prev_xk + self.b)
 
     def check_rel_prec(self, rk):
+        """
+        Checks the convergence criteria by comparing it with the specified
+        relative precision.
+        """
         return self.norm(rk) / self.norm(self.b) < self.rel_prec
 
     @abc.abstractmethod
     def init_Q(self):
+        """
+        Abstract method to define the Q matrix. Each algorithm (method) has to
+        override this method and add its own implementation.
+        """
         self.Q = None
 
 
 class Jacobi(LynSysSolver):
+    """
+    Standard Jacobi method where matrix Q = D.
+    """
     def init_Q(self):
+        """
+        Overrides the Q matrix init with the method specific implementation.
+        """
         self.Q = np.matrix(np.diagflat(np.diag(self.A)))
 
 
 class GaussSeidel(LynSysSolver):
+    """
+    Standard Gauss-Seidel method where matrix Q = D + L.
+    """
     def init_Q(self):
+        """
+        Overrides the Q matrix init with the method specific implementation.
+        """
         self.Q = np.matrix(np.tril(self.A))
 
 
